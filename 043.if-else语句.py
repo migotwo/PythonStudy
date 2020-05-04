4226785e7c9af5458d1a2b912ba6aa47956c6a39
migotwo <lizhenguo28@163.com>
2020-05-04 18:09:02 +0800

    第一创建文件
    第一次使用GITHUB

---
+++ 043.if-else语句.py
@@ -0,0 +1,9 @@
+# 请用户输入一个年龄
+age = input("请输入您的年龄：")
+# 判断用户年龄是否大于18
+if 18 <= int(age) and 30 >= int(age):
+    print("您已经成年了！！")
+elif 30 <= int(age) and  40>= int(age):
+    print("有点大！！！")
+else:
+    print("您还未成年！！")
